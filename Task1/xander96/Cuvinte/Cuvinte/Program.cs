9e663f8272fe01ef1e802b68303b654333212beb
P. Alexandru <petrescu_alexandru96@yahoo.com>
2016-03-01 00:26:05 +0200

    Second upload
    Need to learn how to delete an entire folder...

---
+++ Task1/xander96/Cuvinte/Cuvinte/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuvinte
+{
+    public static class StringExtension
+    {
+        public static string GetLast(this string source, int tail_length)
+        {
+            if (tail_length >= source.Length)
+                return source;
+            return source.Substring(source.Length - tail_length);
+        }
+    }
+    class IN_LITERE
+    {
+        public static string NumberToWords(long number)
+        {
+            if (number == 0)
+                return "";
+
+            if (number < 0)
+                return "minus " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000000) > 0)
+            {
+                if ((number / 1000000000) == 1)
+                {
+                    words += NumberToWords(number / 10000000000) + " un miliard ";
+                }
+                else
+                {
+
+                    if ((number / 1000000000) < 20)
+                        {
+                            words += NumberToWords(number / 1000000000) + " miliarde ";
+                        }
+                    else
+                        {
+                            words += NumberToWords(number / 1000000000) + " de miliarde ";
+                        }
+                }
+                number %= 1000000000;
+            }
+
+            if ((number / 1000000) > 0)
+            {
+                if ((number / 1000000) == 1)
+                {
+                    words += NumberToWords(number / 10000000) + " un milion ";
+                }
+                else
+                {
+                   if ((number / 1000000) < 20 )
+                        {
+                            words += NumberToWords(number / 1000000) + " milioane ";
+                        }
+                    else
+                        {
+                            words += NumberToWords(number / 1000000) + " de milioane ";
+                        }
+                }
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                if ((number / 1000) == 1)
+                {
+                    words += NumberToWords(number / 10000) + " o mie ";
+                }
+                else
+                {
+
+                    if ((number / 1000) < 20)
+                        {
+                            words += NumberToWords(number / 1000) + " mii ";
+                        }
+                    else
+                        {
+                            words += NumberToWords(number / 1000) + " de mii ";
+                        }
+                }
+
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                if ((number / 100) == 1)
+                {
+                    words += NumberToWords(number / 1000) + " o suta ";
+                }
+                else
+                {
+                    words += NumberToWords(number / 100) + " sute ";
+                }
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += " ";
+                var units = new[] { "unu", "doua", "trei", "patru", "cinci", "sase", "sapte", "opt", "noua", "zece" };
+                var unitsMap = new[] { "unu", "doua", "trei", "patru", "cinci", "sase", "sapte", "opt", "noua", "zece", "unspezece", "douasprezece", "treisprezece", "paisprezece", "cincisprezece", "saisprezece", "saptesprezece", "optsprezece", "nouasprezece" };
+                var tensMap = new[] { "zero", "zece", "douazeci", "treizeci", "patruzeci", "cincizeci", "saizeci", "saptezeci", "optzeci", "nouazeci" };
+
+                if (number < 20)
+                    words += unitsMap[number - 1];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += " si " + units[(number % 10) - 1];
+                }
+            }
+            return words;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Urmeaza sa pronuntam numarul: ");
+            long numar=Convert.ToInt64(Console.ReadLine());
+            Console.Write("Numarul dorit este: ");
+            Regex trimmer = new Regex(@"\s\s+");
+            string s = trimmer.Replace(IN_LITERE.NumberToWords(numar), " ");
+            string res = s.GetLast(4);
+            if (string.Compare(res, "doua") == 0)
+            {
+                s = s.Substring(0, s.Length - 2) + "i";
+            }
+            res = s.GetLast(12);
+            if (string.Compare(res, "douasprezece") == 0)
+            {
+                s = s.Substring(0, s.Length - 12) + "doisprezece";
+            }
+            Console.WriteLine(s.ToUpper());
+            Console.WriteLine("Este numarul dorit, nu-i asa? ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task1/xander96/Largest_Number/Largest_Number/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_Number{
+    //In order to sort the numbers properly for their usefull digits we need a special representation
+    //So we are bringing the numbers to the same lenght
+    //For ex. if the numbers are 98 9 976, they will become 989 999 976, so the correct order is 9,98,976
+    class Help
+    {
+        public static int correct_number(int x, int digits)
+        {
+            int correct = new int();
+            correct = x * ((int)(Math.Pow(10, digits - ((int)Math.Floor(Math.Log10(x) + 1)))));
+            for (int i = 0; i < (digits - ((int)Math.Floor(Math.Log10(x) + 1))); i++ )
+            {
+                correct += 9 * (int)(Math.Pow(10, i));
+            }
+            return correct;
+        }
+    }
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We are going to read from input.txt ( The FILE must be in the Debug Folder! )");
+            string fileContent = File.ReadAllText("input.txt");
+            string[] integerStrings = fileContent.Split(new char[] { ' ', '\t', '\r', '\n' , ',' , ';' , ':', '\'', '\"' , '[' , '(' , '{' , ']', ')', '}' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] numbers = new int[integerStrings.Length];
+            for (int n = 0; n < integerStrings.Length; n++)
+                numbers[n] = int.Parse(integerStrings[n]);
+            //Checking if the reading is good
+            /*foreach(int i in numbers)
+            {
+                Console.WriteLine("{0}", i);
+            }*/
+            int max_digits = new int();
+            int aux =  new int();
+            max_digits = 0;
+            foreach(int i in numbers)
+            {
+                if ( max_digits < Math.Floor(Math.Log10(i) + 1) )
+                {
+                    max_digits = (int)Math.Floor(Math.Log10(i) + 1);
+                }
+                if ( max_digits == 5 )
+                {
+                    break;
+                }
+            }
+            //See the max number of digits
+            //Console.WriteLine("{0}", max_digits);
+            //Sorting the Array so we can build the number
+            bool ok = true;
+            while ( ok )
+            {
+                ok = false;
+                for (int i = 0; i < ( numbers.Length - 1 ) ; i++)
+                { 
+                    if ( Help.correct_number(numbers[i],max_digits) < Help.correct_number(numbers[i+1], max_digits))
+                    {
+                        ok = true;
+                        aux = numbers[i];
+                        numbers[i] = numbers[i + 1];
+                        numbers[i + 1] = aux;
+                    }
+                }
+            }
+            Console.Write("Biggest number that can be built is: ");
+            foreach(int i in numbers)
+            {
+                Console.Write("{0}", i);
+            }
+            Console.Read();
+         }
+    }
+}
